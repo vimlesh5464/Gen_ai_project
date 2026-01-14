a8974e9f956b230f8822e7696c9f9d05787efade
vimlesh5464 <vg950683@gmail.com>
2026-01-14 12:12:01 +0530

    Fix deploy: add requirements.txt, rename folder

--- ai_skill_advisor/app.py
+++ ai_skill_advisor/app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from langchain_core.prompts import PromptTemplate
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from dotenv import load_dotenv
 import os
